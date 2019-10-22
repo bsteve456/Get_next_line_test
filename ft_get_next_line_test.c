757143b3d43f06d34bd4dfc01325527a4c68fbb2
bsteve456 <stevealan1@protonmail.com>
2019-10-23 01:07:10 +0200

    add test4

--- ft_get_next_line_test.c
+++ ft_get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: stbaleba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:24:33 by stbaleba          #+#    #+#             */
-/*   Updated: 2019/10/23 00:02:12 by blacking         ###   ########.fr       */
+/*   Updated: 2019/10/23 01:06:24 by blacking         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,20 +47,25 @@ int main()
 	char **line;
 	line = (char **)malloc(sizeof(char *));
 	int fd = 0;
-//	fd = open("./test0", O_RDWR);
-//	printf("---Test_GNL_0---\n");
-//	ft_check_get_new_line(fd, line);
-//	close(fd);
-//	printf("---Test_GNL_1---\n");
-//	fd = open("./test1", O_RDWR);
-//	ft_check_get_new_line(fd, line);
-//	close(fd);
-//	printf("---Test_GNL_2---\n");
-//	fd = open("./test2", O_RDWR);
-//	ft_check_get_new_line(fd, line);
-//	close(fd);
+	fd = open("./test0", O_RDWR);
+	printf("---Test_GNL_0---\n");
+	ft_check_get_new_line(fd, line);
+	close(fd);
+	printf("---Test_GNL_1---\n");
+	fd = open("./test1", O_RDWR);
+	ft_check_get_new_line(fd, line);
+	close(fd);
+	printf("---Test_GNL_2---\n");
+	fd = open("./test2", O_RDWR);
+	ft_check_get_new_line(fd, line);
+	close(fd);
 	printf("---Test_GNL_3---\n");
 	fd = open("./test3", O_RDWR);
 	ft_check_gnl_loop(fd, line);
 	close(fd);
+	printf("---Test_GNL_4---\n");
+	fd = open("./test4", O_RDWR);
+	ft_check_gnl_loop(fd, line);
+	close(fd);
+
 }
