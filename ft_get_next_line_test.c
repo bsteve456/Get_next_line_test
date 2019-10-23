c72dde9e402183f5e082e7f5d82a26134c64b105
Steve-alan BALEBA BALEBA <stbaleba@e1r9p12.42.fr>
2019-10-23 14:31:10 +0200

    modify test

--- ft_get_next_line_test.c
+++ ft_get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: stbaleba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:24:33 by stbaleba          #+#    #+#             */
-/*   Updated: 2019/10/23 10:22:58 by blacking         ###   ########.fr       */
+/*   Updated: 2019/10/23 14:28:03 by stbaleba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,32 +44,35 @@ static void ft_check_gnl_loop(int fd, char **line)
 
 int main()
 {
-	char **line;
-	line = (char **)malloc(sizeof(char *));
+	char *line = NULL;
 	int fd = 0;
-//	fd = open("./test0", O_RDWR);
-//	printf("---Test_GNL_0---\n");
-//	ft_check_get_new_line(fd, line);
-//	close(fd);
-//	printf("---Test_GNL_1---\n");
-//	fd = open("./test1", O_RDWR);
-//	ft_check_get_new_line(fd, line);
-//	close(fd);
-//	printf("---Test_GNL_2---\n");
-//	fd = open("./test2", O_RDWR);
-//	ft_check_get_new_line(fd, line);
-//	close(fd);
-//	printf("---Test_GNL_3---\n");
-//	fd = open("./test3", O_RDWR);
-//	ft_check_gnl_loop(fd, line);
-//	close(fd);
-//	printf("---Test_GNL_4---\n");
-//	fd = open("./test4", O_RDWR);
-//	ft_check_gnl_loop(fd, line);
-//	close(fd);
-	printf("---Test_GNL_5---\n");
-	fd = open("../ft_get_next_line.c", O_RDWR);
-	ft_check_gnl_loop(fd, line);
+	fd = open("./test0", O_RDWR);
+	printf("---Test_GNL_0---\n");
+	ft_check_get_new_line(fd, &line);
 	close(fd);
+	printf("---Test_GNL_1---\n");
+	fd = open("./test1", O_RDWR);
+	ft_check_get_new_line(fd, &line);
+	close(fd);
+	printf("---Test_GNL_2---\n");
+	fd = open("./test2", O_RDWR);
+	ft_check_get_new_line(fd, &line);
+	close(fd);
+	printf("---Test_GNL_3---\n");
+	fd = open("./test3", O_RDWR);
+	ft_check_gnl_loop(fd, &line);
+	close(fd);
+	printf("---Test_GNL_4---\n");
+	fd = open("./test4", O_RDWR);
+	ft_check_gnl_loop(fd, &line);
+	close(fd);
+//	printf("----RANDOM-----\n");
+//	fd = open("/dev/random", O_RDWR);
+//	ft_check_gnl_loop(fd, &line);
+//	close(fd);
+//	printf("---Test_GNL_5---\n");
+//	fd = open("../ft_get_next_line.c", O_RDWR);
+//	ft_check_gnl_loop(fd, line);
+//	close(fd);
 
 }
